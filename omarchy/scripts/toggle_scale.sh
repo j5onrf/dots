cfe4276f9cfe73ff20863e1a170d883ed7b54599
j5onrf <j5onrf1@gmail.com>
2026-05-01 08:33:53 -0500

    Change monitor refresh rate to 120Hz

--- omarchy/scripts/toggle_scale.sh
+++ omarchy/scripts/toggle_scale.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # 2. Apply the monitor scale with your 8-bit preference preserved
-hyprctl keyword monitor "$MONITOR, 3440x1440@75, auto, $NEW_SCALE, bitdepth, 8"
+hyprctl keyword monitor "$MONITOR, 3440x1440@120, auto, $NEW_SCALE, bitdepth, 8"
 
 # 3. Update the GTK files directly
 [ -f "$GTK3_FILE" ] && sed -i "s/^gtk-font-name=.*/gtk-font-name=$FONT_VAL/" "$GTK3_FILE"
